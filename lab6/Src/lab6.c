2670cc259ed044550fe33ca1cbe71582a0993da1
Mohamed Sayed Ahmed <U1621068@utah.edu>
2026-03-06 17:09:52 -0700

    Lab 6: ADC potentiometer LEDs and DAC waveform generation

--- lab6/Src/lab6.c
+++ lab6/Src/lab6.c
@@ -1,85 +1,166 @@
-#include "main.h"
-#include "stm32f0xx_hal.h"
+#include "stm32f0xx.h"
+#include <stdint.h>
 
-void SystemClock_Config(void);
+#define EXERCISE 2   
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+static void delay(volatile uint32_t t)
+{
+    while (t--) __NOP();
+}
+
+#if EXERCISE == 1
+
+
+
+static void leds_init(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+    GPIOC->MODER &= ~((3u << (6 * 2)) |
+                      (3u << (7 * 2)) |
+                      (3u << (8 * 2)) |
+                      (3u << (9 * 2)));
+
+    GPIOC->MODER |=  ((1u << (6 * 2)) |
+                      (1u << (7 * 2)) |
+                      (1u << (8 * 2)) |
+                      (1u << (9 * 2)));
+
+    /* active-low LEDs: 1 = off, 0 = on */
+    GPIOC->ODR |= (1u << 6) | (1u << 7) | (1u << 8) | (1u << 9);
+}
+
+static inline void led_red_on(void)    { GPIOC->ODR &= ~(1u << 6); }
+static inline void led_red_off(void)   { GPIOC->ODR |=  (1u << 6); }
+
+static inline void led_blue_on(void)   { GPIOC->ODR &= ~(1u << 7); }
+static inline void led_blue_off(void)  { GPIOC->ODR |=  (1u << 7); }
+
+static inline void led_orange_on(void) { GPIOC->ODR &= ~(1u << 8); }
+static inline void led_orange_off(void){ GPIOC->ODR |=  (1u << 8); }
+
+static inline void led_green_on(void)  { GPIOC->ODR &= ~(1u << 9); }
+static inline void led_green_off(void) { GPIOC->ODR |=  (1u << 9); }
+
+static void all_leds_off(void)
+{
+    led_red_off();
+    led_blue_off();
+    led_orange_off();
+    led_green_off();
+}
+
+static void adc_init_pc0(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+
+    /* PC0 = analog mode, no pull-up/pull-down */
+    GPIOC->MODER &= ~(3u << (0 * 2));
+    GPIOC->MODER |=  (3u << (0 * 2));
+    GPIOC->PUPDR &= ~(3u << (0 * 2));
+
+    
+    if (ADC1->CR & ADC_CR_ADEN)
+    {
+        ADC1->CR |= ADC_CR_ADDIS;
+        while (ADC1->CR & ADC_CR_ADEN) { }
+    }
+
+    ADC1->CFGR1 = 0;
+    ADC1->CFGR1 |= ADC_CFGR1_CONT;     /* continuous mode */
+    ADC1->CFGR1 |= ADC_CFGR1_RES_0;    /* 8-bit resolution */
+
+    ADC1->CHSELR = ADC_CHSELR_CHSEL10; /* PC0 = ADC_IN10 */
+
+    ADC1->CR |= ADC_CR_ADCAL;
+    while (ADC1->CR & ADC_CR_ADCAL) { }
+
+    ADC1->ISR |= ADC_ISR_ADRDY;
+    ADC1->CR |= ADC_CR_ADEN;
+    while (!(ADC1->ISR & ADC_ISR_ADRDY)) { }
+
+    ADC1->CR |= ADC_CR_ADSTART;
+}
+
+static uint8_t adc_read_8bit(void)
+{
+    return (uint8_t)(ADC1->DR & 0xFF);
+}
+
 int main(void)
 {
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-  /* Configure the system clock */
-  SystemClock_Config();
+    uint8_t adc_val;
 
-  while (1)
-  {
- 
-  }
-  return -1;
+    leds_init();
+    adc_init_pc0();
+
+    while (1)
+    {
+        adc_val = adc_read_8bit();
+        all_leds_off();
+
+        
+        if (adc_val < 64) {
+            led_red_on();
+        } else if (adc_val < 128) {
+            led_blue_on();
+        } else if (adc_val < 192) {
+            led_orange_on();
+        } else {
+            led_green_on();
+        }
+
+        delay(20000);
+    }
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
+#elif EXERCISE == 2
+
+
+
+const uint8_t triangle_table[32] = {
+    0,15,31,47,63,79,95,111,
+    127,142,158,174,190,206,222,238,
+    254,238,222,206,190,174,158,142,
+    127,111,95,79,63,47,31,15
+};
+
+static void dac_init_pa4(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    /* Enable GPIOA clock */
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /* PA4 -> analog mode */
+    GPIOA->MODER &= ~(3u << (4 * 2));
+    GPIOA->MODER |=  (3u << (4 * 2));
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    /* Enable DAC clock */
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /* Enable DAC channel 1 */
+    DAC->CR |= DAC_CR_EN1;
 }
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
+int main(void)
 {
-  /* User can add their own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+    uint32_t i = 0;
+
+    dac_init_pa4();
+
+    while (1)
+    {
+        /* 8-bit right-aligned DAC data */
+        DAC->DHR8R1 = triangle_table[i];
+
+        i++;
+        if (i >= 32) {
+            i = 0;
+        }
+
+        /* about 1 ms delay */
+        delay(48000);
+    }
 }
 
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* User can add their own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-}
-#endif /* USE_FULL_ASSERT */
+#endif
